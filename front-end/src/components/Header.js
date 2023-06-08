7a0a0f2c489a9084218cae5542c7c78f40ae8892
Exorcist <90632640+HeIIBlazer@users.noreply.github.com>
2023-06-08 13:29:21 +0300

    Almost Done with header

--- front-end/src/components/Header.js
+++ front-end/src/components/Header.js
@@ -4,8 +4,14 @@ import NavbarCollapse from 'react-bootstrap/esm/NavbarCollapse';
 import NavbarToggle from 'react-bootstrap/esm/NavbarToggle';
 import logo from '../assets/logo.png';
 
+import axios from 'axios';
+import jwt_decode from 'jwt-decode';
+
 export default function Header() {
    const [color, setColor] = useState(false);
+   const [name, setName] = useState('');
+   let navbar_admin = [];
+
    const changeNavbarColor = () => {
       if (window.scrollY >= 160) {
          setColor(true);
@@ -13,6 +19,27 @@ export default function Header() {
          setColor(false);
       }
    };
+
+   React.useEffect(() => {
+      refreshToken();
+   });
+
+   const refreshToken = async () => {
+      try {
+         const response = await axios.get(`http://localhost:5000/admin/token`);
+         const decoded = jwt_decode(response.data.accessToken);
+         setName(decoded.name);
+      } catch (error) {
+         console.log(error);
+      }
+   };
+
+   if (name !== '') {
+      navbar_admin = [
+         { name: 'Admin Panel', href: '/applications' },
+         { name: 'Выйти', href: '/logout' },
+      ];
+   }
 
    window.addEventListener('scroll', changeNavbarColor);
 
@@ -37,9 +64,13 @@ export default function Header() {
                      <Nav.Link href="#requirements">Условия</Nav.Link>
                      <Nav.Link href="#gallery">Галерея</Nav.Link>
                      <Nav.Link href="#contacts">Контакты</Nav.Link>
-
-                     {/* temporary link */}
-                     <Nav.Link href="/applications">akakakka</Nav.Link>
+                  </Nav>
+                  <Nav>
+                     {navbar_admin.map((item, index) => (
+                        <Nav.Link href={item.href} key={index}>
+                           {item.name}
+                        </Nav.Link>
+                     ))}
                   </Nav>
                </NavbarCollapse>
             </Container>
